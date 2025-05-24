7aec082bfc580b03dd497469976d7125290c5d85
Billy Och <ochiengbilly02@gmail.com>
2025-05-24 10:00:16 +0300

    Add file

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const argument = [];
+
+if(argument.length < 1){
+    console.log('No argument')
+} else if(argument.length === 1){
+    console.log('Argument found')
+}  else if(argument.length > 1){
+    console.log('Arguments found')
+}
